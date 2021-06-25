d325cbb7ed967200953fa637ef165b9e53319da3
shubhendu <shubhendukumars09@gmail.com>
2021-06-25 11:13:39 +0530

    firestore config done for library

---
+++ app.js
@@ -0,0 +1,20 @@
+import firebaseConfig from './config';
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+const booksRef = db.collection('books');
+
+// Subscribes to the db and constantly listens for any changes in the database
+
+// Fetch data
+booksRef.get().then((snapshot) => {
+    const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    }));
+    console.log("All data in 'books' collection", data);
+})
+
+// TODO: Create and Update, Delete pending
